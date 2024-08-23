5d6fbaa3fdcf5cd2a52b8ad03a280a8369960cc3
Stone Pham <tongthachpham22101997@gmail.com>
2024-08-23 14:12:12 +0700

    Bullet
    When the bullet is spawned, it will move vertically until it reaches a position off-screen.

---
+++ Assets/Scripts/MoveObjectWithDOTween.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+/// <summary>
+/// This script is used to move the object to the target position using DOTween.
+/// </summary>
+public class MoveObjectWithDOTween : MonoBehaviour
+{
+    /// <summary>
+    /// Move the object to targetPosition over the duration.
+    /// </summary>
+    /// <param name="targetPosition"></param>
+    /// <param name="duration"></param>
+    public void Move(Vector3 targetPosition, float duration)
+    {
+        // Apply easing InOutQuad to make the movement smoother.
+        transform.DOMove(targetPosition, duration)
+                 .SetEase(Ease.InOutQuad)
+                 .OnComplete(OnMoveComplete);
+    }
+
+    void OnMoveComplete()
+    {
+        transform.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        Move(new Vector3(transform.position.x, 6, 0), 2);
+    }
+}
